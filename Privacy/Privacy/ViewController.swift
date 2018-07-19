67ac40696a3cb62c575b2d12938f503782906583
Zane6w <zhwin@outlook.com>
2018-07-19 16:20:54 +0800

    test 1

---
+++ Privacy/Privacy/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Privacy
+//
+//  Created by zhi zhou on 2018/7/19.
+//  Copyright © 2018年 zhi zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
